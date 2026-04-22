9866fce9d8a66e642c705a6537209ab4d3be0f7e
challavinay251 <challavinay251@gmail.com>
2026-04-22 16:44:09 +0530

    Day 2 update: Homepage UI completed

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from products.models import Product   # make sure this import is there
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    best_sellers = Product.objects.all()[:3]
+
+    return render(request, 'home.html', {
+        'products': products,
+        'best_sellers': best_sellers
+    })
