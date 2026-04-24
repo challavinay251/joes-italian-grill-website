5babe44af9faf6c2f41b7ace667cbdabe03f218a
challavinay251 <challavinay251@gmail.com>
2026-04-24 18:09:28 +0530

    Day 4: Dynamic menu with admin integration

--- core/views.py
+++ core/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from products.models import Category, Product
 
 
-# 🏠 HOME PAGE
 def home(request):
     best_sellers = Product.objects.filter(is_best_seller=True)[:3]
     products = Product.objects.all()[:6]  # for menu preview
@@ -12,8 +11,6 @@ def home(request):
         'products': products
     })
 
-
-# 🍽 MENU PAGE (DYNAMIC)
 def menu(request):
     categories = Category.objects.prefetch_related('products').all()
 
--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -17,17 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-# 🔥 MEDIA IMPORTS (IMPORTANT)
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # 👉 your app urls (example)
-    path('', include('core.urls')),   # or your app name
+    path('', include('core.urls')),
 ]
 
-# 🔥 MEDIA FILES SERVING (FOR IMAGES)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
